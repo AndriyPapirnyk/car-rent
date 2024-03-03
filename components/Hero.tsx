10781665d8bdea9137b7ce1060dd8328b777730e
AndriyPapirnyk <papirnykandriy@gmail.com>
2024-03-03 18:52:51 +0200

    filling main page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,34 @@
+"use client";
+
 import Image from 'next/image';
+import CustomButton from './CustomButton';
 
 
 const Hero = () => {
+    const handleScroll = () => {
+
+    }
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book or rent a car - quickly and easily!
+            </h1>
+
+            <p className="hero__subtitle">
+                Streamline your car rental experience with our effortless booking
+                process.
+            </p>
+
+            <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}  />
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src="/hero.png" fill alt='car image' className='object-contain' />
+                <div className='hero__image-overlay'></div>
+            </div>
+        </div>
+    </div>
   )
 }
 
