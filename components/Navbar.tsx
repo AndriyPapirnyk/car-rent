500c5bdfdca8727d9ba60652763980d3a242b82e
AndriyPapirnyk <papirnykandriy@gmail.com>
2024-03-07 00:29:11 +0200

    styling navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+
+//
+
+import { CustomButton } from '.';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full absolute z-10'>
+        <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
+            <Link href="/" className='flex justify-center items-center'>
+                <Image src="/logo.svg" alt="Logo" width={118} height={18} className='object-contain' />
+            </Link>
+
+            <CustomButton title="Sign In" btnType="button" containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' />
+        </nav>
+    </header>
   )
 }
 
